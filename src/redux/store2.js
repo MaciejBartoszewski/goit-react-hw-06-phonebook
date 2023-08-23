710ab933b93fcc2f2d375d53241055c743d44a2f
Maciej Bartoszewski <bartoszewski.maciek@gmail.com>
2023-08-23 13:25:33 +0200

    commit

--- src/redux/store2.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { contactsReducer } from '../redux/createReducer';
-
-export const store = configureStore({
-  reducer: {
-    contacts: contactsReducer,
-  },
-});
